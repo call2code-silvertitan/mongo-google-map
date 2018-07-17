f05279f0d8387a8d964e7c8b0cc33084260174aa
Jonathan Russell <jdrussell@ibm.com>
2018-07-17 16:59:17 -0400

    Revert "Starting over with something that actually works"
    This reverts commit 6093daf75785e8657057f0741062333fec44c926.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,28 +3,31 @@ import {GeoData} from '../api/database.js';
  
 import './body.html';
  
+Template.body.onCreated(function bodyonCreated(){
+    this.subscribe('geodata');
+})
+
 Template.body.helpers({
     geoData() {
         return GeoData.find({});
     }
 });
 
+
+
 Template.addpointform.events({
     "submit form": function(event, template) {
         event.preventDefault();
         
         GeoData.insert({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
+            location: {
+                type: "Point",
+                coordinates: [
                     Number(template.find(".latitude").value), 
                     Number(template.find(".longitude").value)
                 ]
             },
-            "properties": {
-                "name": template.find(".name").value
-            }
+            title: template.find(".name").value,
         });
     }
 });
