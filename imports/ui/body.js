2a3c25a0144af9e0851ac9c431176a0f3e69e73e
Jonathan Russell <jdrussell@ibm.com>
2018-07-17 16:49:48 -0400

    Revert "Fix updating issues (maybe)"
    This reverts commit ef802229762c68da4edabf76a1942a07e7fbb686.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,31 +3,28 @@ import {GeoData} from '../api/database.js';
  
 import './body.html';
  
-Template.body.onCreated(function bodyonCreated(){
-    this.subscribe('geodata');
-})
-
 Template.body.helpers({
     geoData() {
         return GeoData.find({});
     }
 });
 
-
-
 Template.addpointform.events({
     "submit form": function(event, template) {
         event.preventDefault();
         
         GeoData.insert({
-            location: {
-                type: "Point",
-                coordinates: [
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
                     Number(template.find(".latitude").value), 
                     Number(template.find(".longitude").value)
                 ]
             },
-            title: template.find(".name").value,
+            "properties": {
+                "name": template.find(".name").value
+            }
         });
     }
 });
